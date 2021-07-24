9de19c18986b5bdbbaba8a33aadfcf9e54a1c341
sunhz <sunhz312@163.com>
2021-07-24 11:53:35 +0800

    2-8 add examples of custom configuration items

--- src/main/java/com/mark/wiki/Controller/TestController.java
+++ src/main/java/com/mark/wiki/Controller/TestController.java
@@ -1,5 +1,6 @@
 package com.mark.wiki.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // return string
 // @Controller // return page
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     /**
      * GET, POST, PUT, DELETE
      * <p>
@@ -18,7 +23,7 @@ public class TestController {
 //    @RequestMapping("/hello") // All ways can be accessed.
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
