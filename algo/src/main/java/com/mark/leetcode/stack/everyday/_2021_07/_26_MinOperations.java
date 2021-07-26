b4efde274521b786a539e16c7ceee36497ec3c62
sunhz <sunhz312@163.com>
2021-07-26 23:23:28 +0800

    0726

---
+++ algo/src/main/java/com/mark/leetcode/stack/everyday/_2021_07/_26_MinOperations.java
@@ -0,0 +1,52 @@
+package com.mark.leetcode.stack.everyday._2021_07;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Mark
+ * @date 2021-07-26 23:17
+ */
+public class _26_MinOperations {
+    // target = [6,4,8,1,3,2], arr = [4,7,6,2,3,8,6,1]
+    public int minOperations(int[] target, int[] arr) {
+        int n = target.length;
+        Map<Integer, Integer> pos = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; ++i) {
+            pos.put(target[i], i);
+        }
+        List<Integer> d = new ArrayList<Integer>();
+        for (int val : arr) {
+            if (pos.containsKey(val)) {
+                int idx = pos.get(val);
+                int it = binarySearch(d, idx);
+                if (it != d.size()) {
+                    d.set(it, idx);
+                } else {
+                    d.add(idx);
+                }
+            }
+        }
+        return n - d.size();
+    }
+
+    // TODO 二分查找，注意mid保证不会溢出
+    public int binarySearch(List<Integer> d, int target) {
+        int size = d.size();
+        if (size == 0 || d.get(size - 1) < target) {
+            return size;
+        }
+        int low = 0, high = size - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (d.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
